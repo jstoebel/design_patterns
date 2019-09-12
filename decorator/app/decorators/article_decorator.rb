d1802305ec6216af76c579f131c26d5d10c8b146
Jacob Stoebel <jstoebel@gmail.com>
2019-09-11 21:13:24 -0400

    add article_decorator

--- decorator/app/decorators/article_decorator.rb
+++ decorator/app/decorators/article_decorator.rb
@@ -1,8 +1,8 @@
 class ArticleDecorator < BaseDecorator
   # returns a humanized string describing how long ago the article was published
-  delegate_methods :title
+  delegate_methods :published_at
   include ActionView::Helpers::DateHelper
   def age
-    "Published #{time_ago_in_words(object.published_at)} ago"
+    "Published #{time_ago_in_words(published_at)} ago"
   end
 end
--- decorator/spec/decorators/article_decorator_spec.rb
+++ decorator/spec/decorators/article_decorator_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ArticleDecorator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'exposes the age of the article in human readable format' do
+    decorated = article.decorate
+
+    expect(decorated.age).to eq('Published 1 day ago')
+  end
 end
